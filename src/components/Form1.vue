479d0999b03705cd959428e03a080185cf43fb31
Khoa Le <ltkhoa2711@gmail.com>
2018-11-20 23:34:42 +1100

    fix the forms' format due to misreading the requirements; also add form validations

--- src/components/Form1.vue
+++ src/components/Form1.vue
@@ -1,38 +1,87 @@
 <template>
   <div class="ui centered card">
+    <h1 class="ui header">Page 1</h1>
+
     <div class="content">
-      <div class="ui form">
-        <div class="field">
-          <label>Store Type</label>
-          <select v-model="storeType">
-            <option
-              v-for="(option, index) in storeTypes"
-              :key="index"
-              v-bind:value="option"
-            >
-              {{ option }}
-            </option>
-          </select>
-        </div>
-        <div class="field" v-show="storeType === 'Metro'">
-          <label>Store Detail</label>
-          <input type="text" v-model="storeDetail">
-        </div>
+      <form class="ui form">
+        <!-- Store info -->
+        <fieldset class="ui segment fields">
+          <div class="field">
+            <label>Store Type</label>
+            <select v-model="storeType">
+              <option
+                v-for="(option, index) in storeTypes"
+                :key="index"
+                :value="option"
+              >
+                {{ option }}
+              </option>
+            </select>
+            <div class="ui pointing red basic label" v-show="errors['storeType']">
+              {{ errors['storeType'] }}
+            </div>
+          </div>
+
+          <div class="field" v-show="storeType === 'Metro'">
+            <label>Store Detail</label>
+            <input type="text" v-model="storeDetail">
+            <div class="ui pointing red basic label" v-show="errors['storeDetail']">
+              {{ errors['storeDetail'] }}
+            </div>
+          </div>
+        </fieldset>
+
+        <!-- User info -->
+        <fieldset class="ui segment fields">
+          <div class="field">
+            <label>Name lookup</label>
+            <autocomplete
+              :items="users"
+              @select-result="onFullNameSelected"
+            ></autocomplete>
+            <div class="ui pointing red basic label" v-show="errors['fullName']">
+              {{ errors['fullName'] }}
+            </div>
+          </div>
+
+          <div class="field">
+            <label>First Name</label>
+            <input type="text" v-model="firstName">
+            <div class="ui pointing red basic label" v-show="errors['firstName']">
+              {{ errors['firstName'] }}
+            </div>
+          </div>
+
+          <div class="field">
+            <label>Last Name</label>
+            <input type="text" v-model="lastName">
+            <div class="ui pointing red basic label" v-show="errors['lastName']">
+              {{ errors['lastName'] }}
+            </div>
+          </div>
+        </fieldset>
+
         <div class="ui buttons">
           <button class="ui disabled button" disabled>
             Back
           </button>
-          <button class="ui primary button" v-on:click="next()">
+          <button class="ui primary button" @click="next()">
             Next
           </button>
         </div>
-      </div>
+      </form>
     </div>
   </div>
 </template>
 
 <script>
+import Autocomplete from './Autocomplete';
+
 export default {
+  components: {
+    Autocomplete,
+  },
+
   data() {
     return {
       storeTypes: [
@@ -43,12 +92,59 @@ export default {
       ],
       storeType: '',
       storeDetail: '',
+      users: [],
+      fullName: '',
+      firstName: '',
+      lastName: '',
+      errors: {},       // for form validation
     };
+  },
+
+  mounted() {
+    fetch('https://randomuser.me/api/?results=50&nat=au&exc=login')
+      .then(res => res.json())
+      .then(res => {
+        this.users = res.results.map(u => `${u.name.first} ${u.name.last}`);
+      })
+      .catch(() => {});
   },
 
   methods: {
     next() {
-      this.$router.push('/form2');
+      if (this.validateForm())
+        this.$router.push('/form2');
+    },
+
+    onFullNameSelected(name) {
+      [this.firstName, this.lastName] = name.split(' ', 2);
+    },
+
+    // Validate form and return true if everything is ok
+    validateForm() {
+      this.errors = [];
+
+      if (!this.storeType) {
+        this.errors['storeType'] = 'Store type is required';
+      }
+
+      if (this.storeType === 'Metro' && this.storeDetail.trim() === '') {
+        this.errors['storeDetail'] = 'Store details are required for Metro type';
+      }
+
+      // TODO: fix requirement for full name
+      // if (!this.fullName) {
+      //   this.errors['fullName'] = 'Full name is required';
+      // }
+
+      if (!this.firstName) {
+        this.errors['firstName'] = 'First name is required';
+      }
+
+      if (!this.lastName) {
+        this.errors['lastName'] = 'Last name is required';
+      }
+
+      return Object.keys(this.errors).length === 0;
     },
   },
 }
--- src/components/Form2.vue
+++ src/components/Form2.vue
@@ -1,72 +1,124 @@
 <template>
   <div class="ui centered card">
     <div class="content">
-      <div class="ui form">
+      <form class="ui form">
+        <h2 class="ui dividing header">Page 2</h2>
+
+        <!-- user role -->
         <div class="field">
-          <label>Name lookup</label>
-          <autocomplete
-            :items="users"
-            v-on:select-result="onFullNameSelected"
-          ></autocomplete>
+          <label>What is the user role?</label>
+          <select class="ui fluid dropdown" v-model="role">
+            <option
+              v-for="(role, index) in roles"
+              :key="index"
+              :value="role"
+            >
+              {{ role }}
+            </option>
+          </select>
         </div>
+
+        <!-- first joining date -->
         <div class="field">
-          <label>First Name</label>
-          <input type="text" v-model="firstName">
+          <label>When did the user first join?</label>
+          <input type="date" v-model="joinDate">
+          <div class="ui pointing red basic label" v-show="errors['joinDate']">
+            {{ errors['joinDate'] }}
+          </div>
         </div>
-        <div class="field">
-          <label>Last Name</label>
-          <input type="text" v-model="lastName">
+
+        <!-- is user located in Victoria -->
+        <div class="inline fields">
+          <label>Is this person located in Victoria?</label>
+          <div class="field">
+            <div class="ui radio checkbox">
+              <input
+                type="radio"
+                :value="true"
+                v-model="isLocatedInVic">
+              <label>Yes</label>
+            </div>
+          </div>
+          <div class="field">
+            <div class="ui radio checkbox">
+              <input
+                type="radio"
+                :value="false"
+                v-model="isLocatedInVic">
+              <label>No</label>
+            </div>
+          </div>
+          <div class="ui pointing red basic label" v-show="errors['isLocatedInVic']">
+            {{ errors['isLocatedInVic'] }}
+          </div>
         </div>
+
+        <!-- where in Victoria -->
+        <div class="field" v-show="isLocatedInVic">
+          <label>Where in Victoria?</label>
+          <input type="text" v-model="locationInVic">
+          <div class="ui pointing red basic label" v-show="errors['locationInVic']">
+            {{ errors['locationInVic'] }}
+          </div>
+        </div>
+
         <div class="ui buttons">
-          <button class="ui button" v-on:click="prev()">
+          <button class="ui button" @click="back">
             Back
           </button>
-          <button class="ui primary button" v-on:click="submit()">
-            Next
+          <button class="ui primary button" @click="submit">
+            Submit
           </button>
         </div>
-      </div>
+      </form>
     </div>
   </div>
 </template>
 
 <script>
-import Autocomplete from './Autocomplete';
-
 export default {
   data() {
     return {
-      search: '',
-      users: [],
-      firstName: '',
-      lastName: '',
+      roles: [
+        'Dev',
+        'Manager',
+        'Student',
+      ],
+      role: null,
+      joinDate: null,
+      isLocatedInVic: null,
+      locationInVic: '',
+      errors: {},
     };
   },
 
-  components: {
-    Autocomplete,
-  },
-
-  mounted() {
-    fetch('https://randomuser.me/api/?results=50&nat=au&exc=login')
-      .then(res => res.json())
-      .then(res => {
-        this.users = res.results.map(u => `${u.name.first} ${u.name.last}`);
-      })
-      .catch(() => {});
-  },
-
   methods: {
-    prev() {
+    back() {
       this.$router.go(-1);
     },
 
     submit() {
-      // TODO
+      if (this.validateForm()){
+        // TODO
+      }
     },
 
-    onFullNameSelected(name) {
-      [this.firstName, this.lastName] = name.split(' ', 2);
+    validateForm() {
+      this.errors = [];
+
+      if (!this.joinDate) {
+        this.errors['joinDate'] = 'Joining date is required';
+      }
+
+      if (this.isLocatedInVic === null) {
+        this.errors['isLocatedInVic'] = 'You must select your location';
+      }
+
+      if (!this.locationInVic) {
+        this.errors['locationInVic'] = 'Please specify where in Victoria';
+      }
+
+      return Object.keys(this.errors).length === 0;
     },
   },
 }
